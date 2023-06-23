426c73f5b72ea2251547da58be852ddf66d505f0
Your Name <89849826+vazs12@users.noreply.github.com>
2023-06-23 14:52:53 -0300

    importamos 1 views

--- JovemAcao/sistemaChamada/urls.py
+++ JovemAcao/sistemaChamada/urls.py
@@ -1,8 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path
-
+from .views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns = [
+    path('login', login.html)
+ ]
