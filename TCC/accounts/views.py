e9c50e73e14d7224ba56e77aa2380f7064d179ba
yasmin sampaio <89849826+vazs12@users.noreply.github.com>
2023-06-29 21:42:21 -0300

    Add files via upload

---
+++ TCC/accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('accounts:home')  # Redirecionar para a página inicial após o login
+        else:
+            messages.error(request, 'Credenciais inválidas. Por favor, tente novamente.')
+    return render(request, 'login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registro realizado com sucesso. Faça o login.')
+            return redirect('accounts:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def home_view(request):
+    return render(request, 'home.html')
